ed9fd5537a545bf93c9e6bfb38a95d7af66c6694
millyon <tilahunmillyon@gmail.com>
2025-08-05 16:05:46 +0200

    add webconfig for deployment

---
+++ src/main/java/com/currencyconverter/currency/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.currencyconverter.currency.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Apply CORS only on API paths
+                        .allowedOrigins(
+                                "http://localhost:4200",                     // Angular local dev server
+                                "https://currency-converter-41c0c.web.app/",              // Replace with your deployed frontend URL (Firebase hosting URL)
+                                "https://your-frontend.firebaseapp.com"       // Sometimes Firebase uses this domain too
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // If your frontend sends cookies/auth info
+            }
+        };
+    }
+}
+
